f5d4bb678956ce249a74e7fa4b33b18157db3e5a
Duke Ko <dukhyunko93@gmail.com>
2020-02-04 17:12:13 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
     false
   elsif board[index] == ""
     false
-  elsif board[index] != nil
+  elsif board[index] == nil
     false
   else board[index] == ("X" || "O")
     true
