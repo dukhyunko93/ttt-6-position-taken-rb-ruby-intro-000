e347cde750228320ff9a06c0291389e246aece31
Duke Ko <dukhyunko93@gmail.com>
2020-02-04 17:11:32 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,11 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def position_taken?(board, index)
-  if board[index] != " "
-  elsif board[index] != ""
+  if board[index] == " "
+    false
+  elsif board[index] == ""
+    false
   elsif board[index] != nil
+    false
   else board[index] == ("X" || "O")
+    true
 end
